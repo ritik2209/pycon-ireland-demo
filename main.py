2f18aaac81f28a103b66dfe20d6e18471044012a
Ritik Agarwal <ritik2209@gmail.com>
2022-11-09 12:52:43 +0000

    feat:sklearn added

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 #Importing necessary modules
-import torch
-from fastapi import FastAPI, File, UploadFile
-from PIL import Image
-import torchvision.transforms as transforms
-import torchvision.models as models
-import torch.nn as nn
+from fastapi import FastAPI
+from sklearn.datasets import load_iris
+from pydantic import BaseModel
+import pickle
+
+#Importing the iris dataset
+iris = load_iris()
 
 
-# list of class names by index
-with open("classes.txt") as file:
-    class_names=[line.rstrip('\n') for line in file]
-
-# loading the model architecture
-model = models.resnet50(pretrained=False)
-model.fc=nn.Linear(2048,133,bias=True)
-
-#loading the model weights from the trained model
-model.load_state_dict(torch.load("model_transfer.pt",map_location ='cpu'))
-
-#Model now in evaluation mode (ready for predicting)
-model.eval()
+#Defining the class which takes all the parametes as input
+class request_body(BaseModel):
+    sepal_length : float
+    sepal_width : float
+    petal_length : float
+    petal_width : float
 
 
+#Defining FastAPI Instance
 app = FastAPI(title="Pycon Ireland 2022 Demo")
 
 
@@ -31,37 +26,25 @@ def sum_of_two_numbers(number1: int, number2: int):
     return {"sum": number1+number2}
 
 
-@app.post("/Predict dog breed from image",tags=['Dog Breed Prediction'])
-def predict_dog_breed(img: UploadFile = File(...)):
-    image = Image.open(img.file)
-    predicted_breed=predict_breed(image)
-    return {'Predicted Dog Breed': predicted_breed}
+@app.post("/find_iris_class",tags=['Find iris category'])
+def predict_iris_category(data: request_body):
 
+    # Creating list of list of the values to pass as prarameter to the model
+    test_data = [[
+            data.sepal_length, 
+            data.sepal_width, 
+            data.petal_length, 
+            data.petal_width
+    ]]
 
-def predict_breed(image):
-    # takes image as input and return the predicted breed
+    #Opening the model binary file
+    file = open('iris_model.sav', 'rb')
 
-    # Some transformations to the image before predicting
-    pre_process = transforms.Compose([  transforms.Resize(256),
-                                        transforms.CenterCrop(224),
-                                        transforms.ToTensor(), #converts to tensor
-                                        transforms.Normalize(mean=[0.485, 0.456, 0.406], 
-                                                             std =[0.229, 0.224, 0.225])])
-    image = pre_process(image)
+    #Loading the model into memory for prediction
+    iris_model=pickle.load(file)
 
-    #increases dimension by 1 of the tensor 
-    image.unsqueeze_(dim=0) 
-    
-    # Model predicting on the image tensor
-    output = model(image)
-    
-    # Index which has the max probability for the rprediciton 
-    max_val,index = torch.max(output,dim=1) 
-    
-    
-    predicted_class_index = index.item()
+    #Predicting the class
+    class_idx = iris_model.predict(test_data)[0]
 
-    #Using the index to find the predicted breed name from the list
-    predicted_class_name = class_names[predicted_class_index]
-    
-    return predicted_class_name
+    #Returning the predicted class
+    return { 'Predicted Class' : iris.target_names[class_idx]}
